e27bf52298e240d07e85ce1cef7240222190fd00
James Trauer <james.trauer@monash.edu>
2023-01-13 14:22:31 +1100

    Add 4_03

--- tests/test_external_data/test_vynnycky_white_outputs.py
+++ tests/test_external_data/test_vynnycky_white_outputs.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pathlib import Path
 
 from summer2 import CompartmentalModel
-from summer2.parameters import Parameter
+from summer2.parameters import Parameter, DerivedOutput
 
 
 TEST_OUTPUTS_PATH = Path(__file__).with_name("vynnycky_white_outputs")
@@ -60,7 +60,75 @@ def test_2_08():
     compartments = model.get_outputs_df()
 
     expected_results = pd.read_csv(TEST_OUTPUTS_PATH / "2_08_outputs.csv", index_col=0)
-    model_results = pd.concat((compartments[["Susceptible", "Immune"]], model.get_derived_outputs_df()), axis=1)
+    model_results = pd.concat(
+        (compartments[["Susceptible", "Immune"]], model.get_derived_outputs_df()), axis=1
+    )
+
+    differences = model_results - expected_results
+    assert differences.abs().max().max() < TOLERANCE
+
+
+def test_4_03():
+
+    config = {
+        "total_population": 1e5,
+        "infectious_seed": 1.0,
+        "end_time": 120.0,
+    }
+    parameters = {
+        "r0": 13.0,
+        "latent_period": 7.0,
+        "infectious_period": 8.0,
+    }
+
+    compartments = ("Susceptible", "Pre-infectious", "Infectious", "Immune")
+    model = CompartmentalModel(
+        times=(0.0, config["end_time"]),
+        compartments=compartments,
+        infectious_compartments=["Infectious"],
+    )
+    model.set_initial_population(
+        distribution={
+            "Susceptible": config["total_population"] - config["infectious_seed"],
+            "Infectious": config["infectious_seed"],
+        }
+    )
+    infectious_period = Parameter("infectious_period")
+    model.add_infection_frequency_flow(
+        name="infection",
+        contact_rate=Parameter("r0") / infectious_period,
+        source="Susceptible",
+        dest="Pre-infectious",
+    )
+    model.add_transition_flow(
+        name="progression",
+        fractional_rate=1.0 / Parameter("latent_period"),
+        source="Pre-infectious",
+        dest="Infectious",
+    )
+    model.add_transition_flow(
+        name="recovery", fractional_rate=1.0 / infectious_period, source="Infectious", dest="Immune"
+    )
+    model.request_output_for_flow(name="incidence", flow_name="progression")
+    model.request_output_for_compartments(
+        name="n_suscept",
+        compartments=["Susceptible"],
+        save_results=False,
+    )
+    model.request_function_output(
+        name="suscept_prop", func=DerivedOutput("n_suscept") / config["total_population"]
+    )
+
+    model.run(parameters=parameters, solver="euler")
+
+    expected_results = pd.read_csv(TEST_OUTPUTS_PATH / "4_03_outputs.csv", index_col=0)
+    model_results = pd.concat(
+        (
+            model.get_outputs_df()["Susceptible"] / config["total_population"],
+            model.get_derived_outputs_df()["incidence"],
+        ),
+        axis=1,
+    )
 
     differences = model_results - expected_results
     assert differences.abs().max().max() < TOLERANCE
