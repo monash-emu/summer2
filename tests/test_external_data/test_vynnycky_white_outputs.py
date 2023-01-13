5852b8d3142619762d1c518dc70c144477803a41
James Trauer <james.trauer@monash.edu>
2023-01-13 15:32:50 +1100

    Add one of the models from 4_04

--- tests/test_external_data/test_vynnycky_white_outputs.py
+++ tests/test_external_data/test_vynnycky_white_outputs.py
@@ -156,3 +156,72 @@ def test_4_03():
     )
     differences = model_results - expected_results
     assert differences.abs().max().max() < TOLERANCE
+
+
+def test_4_04():
+
+    config = {
+        "total_population": 1e5,
+        "infectious_seed": 1.,  # Not specified in text
+        "end_time": 120.,
+    }
+    parameters = {
+        "r0": 13.,
+        "latent_period": 7.,
+        "infectious_period": 8.,
+    }
+
+    compartments = (
+        "Susceptible", 
+        "Pre-infectious", 
+        "Infectious", 
+        "Immune"
+    )
+    model = CompartmentalModel(
+        times=(0., config["end_time"]),
+        compartments=compartments,
+        infectious_compartments=["Infectious"],
+    )
+    model.set_initial_population(
+        distribution={
+            "Susceptible": config["total_population"] - config["infectious_seed"],
+            "Infectious": config["infectious_seed"],
+        }
+    )
+    infectious_period = Parameter("infectious_period")
+    model.add_infection_frequency_flow(
+        name="infection", 
+        contact_rate=Parameter("r0") / infectious_period,
+        source="Susceptible",
+        dest="Pre-infectious"
+    )
+    model.add_transition_flow(
+        name="progression", 
+        fractional_rate=1. / Parameter("latent_period"),
+        source="Pre-infectious", 
+        dest="Infectious"
+    )
+    model.add_transition_flow(
+        name="recovery", 
+        fractional_rate=1. / infectious_period,
+        source="Infectious", 
+        dest="Immune"
+    )
+    model.request_output_for_flow(
+        name="incidence", 
+        flow_name="progression"
+    )
+
+    model.run(parameters=parameters, solver="euler")
+
+    expected_results = pd.read_csv(TEST_OUTPUTS_PATH / "4_04_measles_outputs.csv", index_col=0)
+    model_results = pd.concat(
+        (
+            model.get_outputs_df()["Susceptible"] / config["total_population"], 
+            model.get_outputs_df()["Immune"] / config["total_population"], 
+            model.get_derived_outputs_df()["incidence"],
+        ), 
+        axis=1
+    )
+    differences = model_results - expected_results
+    assert differences.abs().max().max() < TOLERANCE
